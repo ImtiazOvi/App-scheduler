13d05def272930da7e475ec2575bad6172b34cb0
imtiazovi020 <imtiazuddin018@gmail.com>
2022-03-17 02:16:58 +0600

    Core/build Src added

---
+++ buildSrc/src/main/java/AppDep.kt
@@ -0,0 +1,12 @@
+object AppDep {
+    const val appCompat = "androidx.appcompat:appcompat:${DepVersion.appcompatVersion}"
+    const val coreKtx = "androidx.core:core-ktx:${DepVersion.coreKtxVersion}"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:${DepVersion.constraintLayoutVersion}"
+    const val material = "com.google.android.material:material:${DepVersion.materialVersion}"
+    const val fragment = "androidx.fragment:fragment-ktx:${DepVersion.fragmentVersion}"
+    const val activity = "androidx.activity:activity-ktx:${DepVersion.activityVersion}"
+
+    const val junit = "junit:junit:${DepVersion.junitVersion}"
+    const val extJunit = "androidx.test.ext:junit:${DepVersion.junitExtVersion}"
+    const val espresso = "androidx.test.espresso:espresso-core:${DepVersion.espressoVersion}"
+}
---
+++ buildSrc/src/main/java/DepVersion.kt
@@ -0,0 +1,15 @@
+object DepVersion {
+    const val buildGradleVersion = "7.0.3"
+    const val kotlinVersion = "1.6.0"
+
+    const val appcompatVersion = "1.3.1"
+    const val coreKtxVersion = "1.6.0"
+    const val constraintLayoutVersion = "2.1.1"
+    const val materialVersion = "1.4.0"
+    const val fragmentVersion = "1.4.0"
+    const val activityVersion = "1.4.0"
+
+    const val junitVersion = "4.13.2"
+    const val junitExtVersion = "1.1.3"
+    const val espressoVersion = "3.4.0"
+}
